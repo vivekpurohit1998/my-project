dbbac034c8488212ebe61ba765881895acaac0a0
Vivek purohit <58132570+vivekpurohit1998@users.noreply.github.com>
2019-11-25 15:01:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+req = requests.get('https://www.bewakoof.com/desi-collection')
+#print(req.status_code)
+#print(req.content)
+
+bs = BeautifulSoup(req.content,'html.parser')
+
+# print(bs.prettify())
+fp = open('file_name.csv','w')
+fp.write('tshirt name,price,image\n')
+
+for product in bs.find_all('div',{'class': 'productCardBox'}):
+  for detail in product.find_all('div',{'class':'productCardDetail'}):
+    print(detail.find_all('h3'))
+    fp.write(str(detail.find_all('h3')[0].text))
+    fp.write(',')
+    print(detail.find_all('b'))
+    fp.write(str(detail.find_all('b')[1].text))
+    fp.write(',')
+  for product in bs.find_all('div',{'class': 'productCardBox'}):
+    for box in product.find_all('div',{'class': 'productCardImg'}):
+      for imgs in box.find_all('img'):
+        print(imgs.get('src'))
+        fp.write(imgs.get('src'))
+        fp.write(',')
+        fp.write('\n')
+        break
+      break
+    break
+  
+    
+fp.close()      
+
